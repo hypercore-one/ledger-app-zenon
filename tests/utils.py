bcce9c698dbad8b509cce376e50e73b68b39fc57
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2024-04-30 15:30:09 +0200

    Fix stax tests

--- tests/utils.py
+++ tests/utils.py
@@ -4,6 +4,7 @@ from hashlib import sha512
 from ecdsa.curves import Ed25519
 from ecdsa.keys import VerifyingKey
 
+ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
 
 # Check if a signature of a given message is valid
 def check_signature_validity(public_key: bytes, signature: bytes, message: bytes) -> bool:
